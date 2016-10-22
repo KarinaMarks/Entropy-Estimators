160cdb58c38f5e66677d9d071f02bf6613ab0f90
GemmaGibbs1234 <lola1963@hotmail.co.uk>
2016-10-22 14:05:30 +0100

    rearranging functions

--- EntropyEst/R/KLEE.R
+++ EntropyEst/R/KLEE.R
@@ -3,22 +3,10 @@
 #' Uses the kth Nearest Neighbour method, expanded by Kozachenko and Leonenko to generate a constsitent and asymptotically unbiased estimator for entropy
 #' Only considers the 1 dimensional case
 #' @param X a vector of samples from a known/unknown distribution
-#'        k the degree of nearest neigbour to estimate by
+#' @param k the degree of nearest neigbour to estimate by
 #' @export
 #' @import knn.dist from FNN
 
-# The Gamma function
-GammaFun <- function(m) {
-  # check that m > 0, if it is not - then this will not work
-  # do not need this specifically for the kth nn as the argument in gamma is always 1 + dimension/2 which is always positive
-  stopifnot(m > 0)
-  # writing the function for the integrand
-  integrand <- function(x) {x^(m-1)*exp(-x)}
-  # integrating the integrand from 0 to infinity
-  res <- integrate(integrand, 0, Inf)
-  # selecting the result of the integral from the integrate class, which is a list structure 
-  as.numeric(res$value)
-}
 
 
 # The volume of the unit 1-dimensional Euclidean ball
