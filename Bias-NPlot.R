53bd85f81820583b9e92a0605131b4914c6e4551
KarinaMarks <karina@hardybee.com>
2016-10-23 12:01:44 +0100

    Explored linear regression

--- Bias-NPlot.R
+++ Bias-NPlot.R
@@ -2,11 +2,12 @@ library(ggplot2)
 
 # plot of Bias against N
 sd <- 0.5
+k <- 1
 N <- seq(500, 10000, 500)
 Bias <- numeric(0)
 
 for (i in 1:length(N)){
-  Bias[i] <- EntBias(rnorm(N[i], sd=sd), k=1, sd=sd)
+  Bias[i] <- EntBias(rnorm(N[i], sd=sd), k=k, sd=sd)
 }
 
 g <- ggplot() +
@@ -17,6 +18,9 @@ g
 # plot of log(Bias) against log(N)
 
 glog <- ggplot() +
-  geom_point(aes(x=log(N), y=log(Bias)))
+  geom_point(aes(x=log(N), y=log(Bias))) +
+  theme(axis.line = element_line(colour = "black"))
 
 glog
+
+reg <- lm(log(Bias) ~ log(N))
