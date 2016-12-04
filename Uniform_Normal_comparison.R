96e237ac3792caa150d2f47e2cc24ea6b80208bb
KarinaMarks <karina@hardybee.com>
2016-12-04 17:45:29 +0000

    Added exponential data

--- Uniform_Normal_comparison.R
+++ Uniform_Normal_comparison.R
@@ -6,8 +6,13 @@ df2 <- data.frame(k = c(1, 2, 3, 5, 10),
                  a = -c(0.3698, 0.5857, 0.6291, 0.7501, 1.0357), 
                  c = c(log(0.0103), log(0.0503), log(0.0737), log(0.1889), log(3.8217)))
 
+df3 <- data.frame(k = c(1, 2, 3, 5, 10),
+                  a = -c(0.4147, 0.6480, 0, 0, 0),
+                  c = c(log(0.0198), log(0.1482), log(1), log(1), log(1)))
+
 df1$dist <- "normal"
 df2$dist <- "uniform"
+df3$dist <- "exponential"
 
 df <- dplyr::bind_rows(df1, df2)
 
@@ -26,8 +31,10 @@ g <- ggplot()+
 
 g
 
+somek <- 5
+
 dfk1 <- df %>%
-  filter(k==1)
+  filter(k==somek)
 
 g <- ggplot()+
   geom_abline(aes(intercept=c, slope=a, colour=dist), data=dfk1, size=1) +
@@ -35,6 +42,6 @@ g <- ggplot()+
   ylim(-15, 0) +
   xlab("log(N)") +
   ylab("log(Bias(H))") +
-  ggtitle("Comparison of the regression lines for the
-          Normal and Uniform distributions (k=1)")+ 
+  ggtitle(paste("Comparison of the regression lines for the
+          Normal and Uniform distributions (k=", somek, ")"))+ 
   labs(colour = "Distribution")
