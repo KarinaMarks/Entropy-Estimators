6174bafcc6fd1ffbad1aa75645b7bb08ff8d87d7
KarinaMarks <karina@hardybee.com>
2017-01-29 20:17:54 +0000

    added some stuff

--- OtherCode/NormalGraphs.R
+++ OtherCode/NormalGraphs.R
@@ -16,12 +16,24 @@ graph <- ggplot(aes(x=n, y=k1), data=logdata) +
   geom_smooth(method='lm', se = FALSE) +
   ggtitle(paste0("Simulation from the Normal distribution,
 for bias of the K-L entropy estimator at varying sample sizes,
-up to N = 50,000 for the kth nearest neighbour (k =1")) +
+up to N = 50,000 for the kth nearest neighbour (k=1")) +
   xlab("log(N)") +
   ylab("log(Bias(H))")
 
 # find linear relationship of logarithm of bias against logrithm of n
-lm(logdata$n ~ logdata$k1)
+reg <- lm(logdata$n ~ logdata$k1)
+
+# the coeffs
+c <- round(exp(reg$coefficients[["(Intercept)"]]), 4)
+a <- round(-reg$coefficients[["logdata$k1"]], 4)
+
+#TODO - find wassup with c?
 
 # find correlation coefficient
-cor(logdata$n, logdata$k1)
+corcoef <- cor(logdata$n, logdata$k1)
+
+# create a df with everything on
+normalInfo <- data.frame(k = 1:11, a = rep(0, 11), c = rep(0, 11), corr = rep(0, 11))
+
+# fill in the first row for k=1
+normalInfo[1,] <- c(1, a, c, corcoef)
