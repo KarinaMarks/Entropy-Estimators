a8385808d1c6e7b27023e09bf4f854dd4a370522
KarinaMarks <karina@hardybee.com>
2017-04-10 10:36:42 +0100

    slight changes made to absrtact and appendix A. Write up complete

--- WriteUp/Code/Rho.R
+++ WriteUp/Code/Rho.R
@@ -4,10 +4,8 @@ Rho <- function(X, k, d=1) {
     n <- length(X)
     # check that k is not larger than the length of the vector
     stopifnot(n > k)
-    
     # creating the matrix of kth nn distances for X
     NNdist <- FNN::knn.dist(data=X, k=k)
-    
     # return the kth column of the matrix
     NNdist[,k]
   } else {
